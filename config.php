e1d3e4d52bf3bfb6b90515539bd400a41efbe93a
mikelietz <codger+git@gmail.com>
2012-04-19 11:13:52 -0400

    Trivial change to test if this is going to go live.

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php if ( !defined( 'HABARI_PATH' ) ) { die( 'No direct access' ); }
 Config::set( 'db_connection', array(
+
 	'connection_string'=>'mysql:host=a.db.shared.orchestra.io;dbname=db_63131071',
 	'username'=>'user_63131071',
 	'password'=>'P%jgWeEY!t!mDn',

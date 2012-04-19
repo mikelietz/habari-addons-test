c9b39d8611b3ec15b2cbdbc6265bdf5c38e8edca
mikelietz <codger+git@gmail.com>
2012-04-19 10:58:52 -0400

    My config.

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php if ( !defined( 'HABARI_PATH' ) ) { die( 'No direct access' ); }
 Config::set( 'db_connection', array(
-	'connection_string'=>'mysql:host=a.db.shared.orchestra.io;dbname=db_0b3f1679',
-	'username'=>'user_0b3f1679',
-	'password'=>'O)x6ju_7eaU.Fg',
+	'connection_string'=>'mysql:host=a.db.shared.orchestra.io;dbname=db_63131071',
+	'username'=>'user_63131071',
+	'password'=>'P%jgWeEY!t!mDn',
 	'prefix'=>''
 ));
 define('DEBUG', true);
